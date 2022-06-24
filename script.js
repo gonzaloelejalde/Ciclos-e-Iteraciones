170955897d19faa8965ecd413b07ea1dd8b9a9e0
Asdrúbal de Elejalde <asdrubalolocco@gmail.com>
2022-06-24 19:03:49 -0300

    strings

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ do {
     numero1 = parseFloat(prompt("Ingrese su peso"))
     operacion =parseInt(prompt("Ingrese 1 para Marte y 2 para Jupiter"))
     peso = parseInt(numero1)
-    gravedad_en_la_tierra = 9.8
-    gravedad_en_marte = 3.7
-    gravedad_en_jupiter = 24.8
+    gravedad_en_la_tierra = "9.8"
+    gravedad_en_marte = "3.7"
+    gravedad_en_jupiter = "24.8"
 
     if(operacion == 1){peso_final = peso * gravedad_en_marte / gravedad_en_la_tierra; alert("Tu peso en Marte es de " + peso_final + " Kgs") }
     else if(operacion == 2){peso_final = peso * gravedad_en_jupiter / gravedad_en_la_tierra; alert("Tu peso en Jupiter es de " + peso_final + " Kgs")}
@@ -14,6 +14,8 @@ do {
     else {peso_final = isNaN; nombre = "Error"}
     peso_final = parseInt(peso_final);
 
-    }while(isNaN(numero1) || isNaN(operacion) || operacion >= 3)
+    }while(isNaN(numero1) || isNaN(operacion) || operacion >= "3")
+
+
 
 console.log(peso_final)
