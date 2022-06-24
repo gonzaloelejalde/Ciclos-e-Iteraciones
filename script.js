3e7c66b4e666ee9387b1b717df762aa21f83a3dd
Asdrúbal de Elejalde <asdrubalolocco@gmail.com>
2022-06-24 19:08:45 -0300

    strings

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ do {
     gravedad_en_marte = "3.7"
     gravedad_en_jupiter = "24.8"
 
-    if(operacion == 1){peso_final = peso * gravedad_en_marte / gravedad_en_la_tierra; alert("Tu peso en Marte es de " + peso_final + " Kgs") }
-    else if(operacion == 2){peso_final = peso * gravedad_en_jupiter / gravedad_en_la_tierra; alert("Tu peso en Jupiter es de " + peso_final + " Kgs")}
-    else if(operacion >= 3) {alert ("Ingrese un número válido, reinicie y pruebe de nuevo")}
+    if(operacion == "1"){peso_final = peso * gravedad_en_marte / gravedad_en_la_tierra; alert("Tu peso en Marte es de " + peso_final + " Kgs") }
+    else if(operacion == "2"){peso_final = peso * gravedad_en_jupiter / gravedad_en_la_tierra; alert("Tu peso en Jupiter es de " + peso_final + " Kgs")}
+    else if(operacion >= "3") {alert ("Ingrese un número válido, reinicie y pruebe de nuevo")}
     else {peso_final = isNaN; nombre = "Error"}
     peso_final = parseInt(peso_final);
 
