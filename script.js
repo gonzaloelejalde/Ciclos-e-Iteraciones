16bc211d637cbdf0ba4fbd593d58255f53dcd2d0
Asdrúbal de Elejalde <asdrubalolocco@gmail.com>
2022-06-27 13:49:08 -0300

    Codigo corregido

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ do {
     gravedad_en_la_tierra = "9.8"
     gravedad_en_marte = "3.7"
     gravedad_en_jupiter = "24.8"
+    }while(isNaN(numero1) || isNaN(operacion) || operacion >= "3")
 
     if(operacion == "1"){peso_final = peso * gravedad_en_marte / gravedad_en_la_tierra; alert("Tu peso en Marte es de " + peso_final + " Kgs") }
     else if(operacion == "2"){peso_final = peso * gravedad_en_jupiter / gravedad_en_la_tierra; alert("Tu peso en Jupiter es de " + peso_final + " Kgs")}
     else if(operacion >= "3") {alert ("Ingrese un número válido, reinicie y pruebe de nuevo")}
     else {peso_final = isNaN; nombre = "Error"}
     peso_final = parseInt(peso_final);
-
-    }while(isNaN(numero1) || isNaN(operacion) || operacion >= "3")
-
-
 
 console.log(peso_final)
